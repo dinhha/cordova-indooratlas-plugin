564fbe75ad6000254c9314fed0a541dd4f26883e
dinhha <dinhha_2000@yahoo.com.vn>
2016-05-15 12:01:37 +0700

    ''

--- www/indoorAtlas.js
+++ www/indoorAtlas.js
@@ -1,19 +1,19 @@
-cordova = require('cordova');
+var exec = require('cordova/exec');
 
 function IndoorAtlas() {
 
 }
 
 IndoorAtlas.prototype.init = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "IndoorAtlas", "init", []);
+    exec(successCallback, errorCallback, "IndoorAtlas", "init", []);
 };
 
 IndoorAtlas.prototype.getCurrentLocation = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "IndoorAtlas", "getCurrentLocation", []);
+    exec(successCallback, errorCallback, "IndoorAtlas", "getCurrentLocation", []);
 };
 
 IndoorAtlas.prototype.setLocationUpdateHandler = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "IndoorAtlas", "setLocationUpdateHandler", []);
+    exec(successCallback, errorCallback, "IndoorAtlas", "setLocationUpdateHandler", []);
 };
 
 module.exports = new IndoorAtlas();
